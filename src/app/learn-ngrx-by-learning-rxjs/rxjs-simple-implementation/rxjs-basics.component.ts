4e74f0272dd8eb03143bdfdc00a9785fa4855e83
lironh <lironenk@gmail.com>
2018-04-27 16:46:10 +0300

    added UI

--- src/app/learn-ngrx-by-learning-rxjs/rxjs-simple-implementation/rxjs-basics.component.ts
+++ src/app/learn-ngrx-by-learning-rxjs/rxjs-simple-implementation/rxjs-basics.component.ts
@@ -9,6 +9,8 @@ import {MyObservable} from './data-producer.service';
 export class RxjsBasicsComponent implements OnInit {
 
   constructor() {}
+  val1;
+  val2;
 
   ngOnInit() {
     const myComponentObserver = {
@@ -17,9 +19,15 @@ export class RxjsBasicsComponent implements OnInit {
       complete: () => console.log(`Completed!`),
     };
     const obs1$ = new MyObservable(myComponentObserver);
-    const subscription$ = obs1$.subscribe(val => console.log(`Observer 1... ${val}`));
+    const subscription$ = obs1$.subscribe(val => {
+      this.val1 = val;
+      console.log(`Observer 1... ${val}`;
+    });
     const obs2$ = new MyObservable({next: val => val});
-    obs2$.subscribe(val => console.log(`Observer 2... ${val}`));
+    obs2$.subscribe(val =>  {
+      this.val2 = val;
+      console.log(`Observer 2... ${val}`;
+    });
     setTimeout(() => {
       subscription$.unsubscribe();
     }, 3000);
